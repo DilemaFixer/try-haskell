5489e195a5a9eaf9f13845d5ce6fd9eba488d3ba
Illa Shisko <109586143+DilemaFixer@users.noreply.github.com>
2025-04-25 13:31:42 +0200

    End with statments

--- statments/st.hs
+++ statments/st.hs
@@ -3,7 +3,7 @@ module Main(main) where
 foo :: Int -> Int 
 foo num = num * num
 
-getStringByNum :: Int -> String 
+{- getStringByNum :: Int -> String 
 getStringByNum 0 = "Num is zero"
 getStringByNum 1 = "Num is one"
 getStringByNum a = 
@@ -12,7 +12,23 @@ getStringByNum a =
     else 
       if a < 50 
       then "Num is less then 50"
-      else "Num is more than 50"
+      else "Num is more than 50" -}
+
+{- getStringByNum :: Int -> String 
+getStringByNum num
+    | num < 50 = "Less then 50"
+    | num > 50 = "More then 50"
+    | num == 50 = "Is 50" -} 
+
+{- getStringByNum :: Int -> String 
+getStringByNum num =  
+    case num of 
+      50 -> "Is 50"
+      49 -> "Is 49"
+      51 -> "Is 51" 
+      _ -> "Other" -} 
+
+
 
 main :: IO ()
-main = putStrLn $ getStringByNum 50 
+main = putStrLn $ getStringByNum 50

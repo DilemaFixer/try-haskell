53c1e4102c282226ec148c480e6ff6a73b59939e
Illa Shisko <109586143+DilemaFixer@users.noreply.github.com>
2025-04-26 15:23:02 +0200

    Remove some staff

--- txt-analyzer/analyzer.hs
+++
@@ -1,9 +0,0 @@
-module Main(main) where 
-import System.Environment (getArgs)
-
-main :: IO ()
-main = do 
-    args <- getArgs
-    case args of 
-        [] -> error "Must have path to txt file"
-        (x:_) -> putStrLn $ "Will parse file " ++ show x
